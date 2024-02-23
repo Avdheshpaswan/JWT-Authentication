b011a9a6eff889e859898062dbba8e253cb99037
Avdhesh <114665510+Avdheshpaswan@users.noreply.github.com>
2024-02-23 20:08:53 +0000

    Sign up and login endpoint defined with dependency

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import uvicorn 
-from fastapi import FastAPI,HTTPException
-from app.model import PostSchema
+from fastapi import FastAPI,HTTPException,Body,Depends
+from app.model import PostSchema,UserSchema,UserLoginSchema
+from app.auth.jwt_handler import signJWT
+from app.auth.jwt_bearer import JWTBearer
 
 posts=[
     {
@@ -24,7 +26,7 @@ posts=[
         "content":"advancement in latest tech world has boosted the development multifolds in future"
     }    
 ]
-
+users=[]
 app=FastAPI()
 
 # get endpoint for testing
@@ -51,9 +53,31 @@ def get_specific_post(id:int):
 
 
 #post endpoint to create newpost
-@app.post("/posts",tags=["posts"])
+@app.post("/posts",dependencies=[Depends(JWTBearer())],tags=["posts"])
 def add_post(post:PostSchema):
     post.id=len(posts)+1;
     posts.append(post.__dict__)
 
     return {"msg":"New post added"}  
+
+#user Sign up(create new user)
+@app.post("/post/signup",tags=["user"])
+def sign_up(user:UserSchema=Body(default=None)):
+    users.append(user)
+    return signJWT(user.email)
+
+def check_user(data:UserLoginSchema):
+    for user in users:
+        if user.email==data.email and user.password==data.password:
+            return True
+        return False
+    
+#user login
+@app.post("/post/login",tags=["user"])
+def sign_up(user:UserLoginSchema=Body(default=None)):
+    if check_user(user):
+        return signJWT(user.email)
+    else:
+        return{
+            "error":"Invalid login details"
+        }
