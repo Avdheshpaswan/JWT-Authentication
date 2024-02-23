aa739c34c86222aaf923c13ea5408e69255700ba
Avdhesh <114665510+Avdheshpaswan@users.noreply.github.com>
2024-02-23 20:12:18 +0000

    UserSchem and UserLoginSchema deined for authentication

--- app/model.py
+++ app/model.py
@@ -12,3 +12,31 @@ class PostSchema(BaseModel):
                 "content":"Content related to post"
             }
         }
+
+class UserSchema(BaseModel):
+    fullname:str=Field(default=None)
+    email:str=Field(default=None)
+    password:str=Field(default=None)
+
+
+    class config:
+        user_schema={
+            "user_dummy":{
+                "fullname":"abc",
+                "email":"abc@gmail.com",
+                "password":"123@hello"   
+            }
+        }
+
+
+class UserLoginSchema(BaseModel):
+    email:str=Field(default=None)
+    password:str=Field(default=None)
+
+    class config:
+        user_schema={
+            "user_dummy":{
+                "email":"abc@gmail.com",
+                "password":"123@hello"   
+            }
+        }        
